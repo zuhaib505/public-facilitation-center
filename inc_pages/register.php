aa4608f08af9529ab9cfc64c2fa7d0c6edd3e4c4
Zuhaib Hassan <84366485+zuhaib505@users.noreply.github.com>
2022-01-23 19:45:40 +0500

    Add Register Page

---
+++ inc_pages/register.php
@@ -0,0 +1,66 @@
+        <!--====== Start breadcrumbs-area Section ======-->
+        <section class="breadcrumbs-area bg_cover" style="background-image: url(assets/images/bg/breadcrumb-bg-1.jpg);">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="breadcrumbs-content text-center">
+                            <h1>Contact Us</h1>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li class="active">Contact Us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End breadcrumbs-area Section ======-->
+        <!--====== Start Contact-area Section ======-->
+        <section class="contact-area bg_cover pt-120 pb-120" style="background-image: url(assets/images/bg/contact-bg-1.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-3 col-lg-6">
+                        <div class="contact-wrapper">
+                            <div class="section-title mb-40">
+                                <h3>Register Yourself</h3>
+                            </div>
+                            <form class="contact-form card p-5">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="text" class="form_control" placeholder="Enter Name" name="name" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="email" class="form_control" placeholder="Enter Email" name="email" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">  
+                                        <div class="form-group">
+                                            <input style="height:46px;border-radius: 0px;" type="file" class="form-control" name="profile_image" id="profile_image" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="password" class="form_control" placeholder="password" name="password" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="password" class="form_control" placeholder="c_password" name="c_password" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_button">
+                                            <button class="main-btn">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End Contact-area Section ======-->

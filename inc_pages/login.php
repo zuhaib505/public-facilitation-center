1e9364d71132bf02fccbe644b055b81ce7693b4c
ghayoor289 <92662749+ghayoor289@users.noreply.github.com>
2022-01-23 19:53:21 +0500

    Add Login Page

---
+++ inc_pages/login.php
@@ -0,0 +1,51 @@
+        <!--====== Start breadcrumbs-area Section ======-->
+        <section class="breadcrumbs-area bg_cover" style="background-image: url(assets/images/bg/breadcrumb-bg-1.jpg);">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10">
+                        <div class="breadcrumbs-content text-center">
+                            <h1>Login To Get Access To Your Account</h1>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li class="active">Login</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End breadcrumbs-area Section ======-->
+        <!--====== Start Contact-area Section ======-->
+        <section class="contact-area bg_cover pt-120 pb-120" style="background-image: url(assets/images/bg/contact-bg-1.jpg);">
+            <div class="container">
+                <div class="row">
+                    <div class="offset-3 col-lg-6">
+                        <div class="contact-wrapper">
+                            <div class="section-title mb-40">
+                                <h3>Login To Get Access To Your Account</h3>
+                            </div>
+                            <form class="contact-form card p-5">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="email" class="form_control" placeholder="Enter Email" name="email" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_group">
+                                            <input type="password" class="form_control" placeholder="password" name="password" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form_button">
+                                            <button class="main-btn">Login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End Contact-area Section ======-->
